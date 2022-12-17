7e2ca891a1f609d4b54f7f2e58050cfc90445688
DevExpressExampleBot <fakeEmail>
2022-12-17 13:05:36 +0400

    Source auto update [skip ci]

---
+++ VB/CarouselItemMovingPath/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace CarouselItemMovingPath
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
